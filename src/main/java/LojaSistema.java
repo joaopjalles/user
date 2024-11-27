9c868d1aab972739dcfe3f26c1f499147a6a5c2c
joaopjalles <jpedrojalles@gmail.com>
2024-11-27 17:59:48 -0300

    att1

--- src/main/java/LojaSistema.java
+++ src/main/java/LojaSistema.java
@@ -1,23 +1,23 @@
-public class LojaSistema {
-    public static void main(String[] args) {
-
-        Role adminRole = new Role("Admin");
-        Role caixaRole = new Role("Caixa");
-        Role clienteRole = new Role("Cliente");
-
-        User user1 = new User("joao123", "joao@gmail.com");
-        User user2 = new User("maria456", "maria@hotmail.com");
-
-        user1.addRole(adminRole);
-        user1.addRole(caixaRole);
-
-        user2.addRole(clienteRole);
-
-        System.out.println(user1);
-        System.out.println(user2);
-
-        user1.removeRole(caixaRole);
-        System.out.println("Após remover o papel de Caixa do usuário 1:");
-        System.out.println(user1);
-    }
-}
+//public class LojaSistema {
+//    public static void main(String[] args) {
+//
+//        Role adminRole = new Role("Admin");
+//        Role caixaRole = new Role("Caixa");
+//        Role clienteRole = new Role("Cliente");
+//
+//        User user1 = new User("joao123", "joao@gmail.com");
+//        User user2 = new User("maria456", "maria@hotmail.com");
+//
+////        user1.addRole(adminRole);
+////        user1.addRole(caixaRole);
+////
+////        user2.addRole(clienteRole);
+////
+////        System.out.println(user1);
+////        System.out.println(user2);
+////
+////        user1.removeRole(caixaRole);
+////        System.out.println("Após remover o papel de Caixa do usuário 1:");
+////        System.out.println(user1);
+////    }
+//}
